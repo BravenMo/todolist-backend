053d6e7a104bcedbd6bb22409c7f8b9317858b22
BravenMo <Varmamohit53@gmail.com>
2025-04-22 00:59:01 +0530

    Added health check

--- src/main/java/com/backend/todolistbackend/controllers/ToDoController.java
+++ src/main/java/com/backend/todolistbackend/controllers/ToDoController.java
@@ -1,1 +1,13 @@
+package com.backend.todolistbackend.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ToDoController {
+
+    @GetMapping("/")
+    public String healthCheck(){
+        return "Spring Boot App is working";
+    }
+}
