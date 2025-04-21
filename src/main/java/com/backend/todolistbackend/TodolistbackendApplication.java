ce42b2e492031b82a992370e060e2fa5ca0bfa32
BravenMo <Varmamohit53@gmail.com>
2025-04-22 00:46:28 +0530

    Updated logging

--- src/main/java/com/backend/todolistbackend/TodolistbackendApplication.java
+++ src/main/java/com/backend/todolistbackend/TodolistbackendApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodolistbackendApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Starting Application: ");
-		System.out.println("🔍 DB_HOST: " + System.getenv("DB_HOST"));
-		System.out.println("🔍 JDBC URL: " + System.getenv("DB_HOST") + ":" + System.getenv("DB_PORT") + "/" + System.getenv("DB_NAME"));
 		SpringApplication.run(TodolistbackendApplication.class, args);
 	}
 
